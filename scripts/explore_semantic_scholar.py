e4373ddc42bbc50f1efe7f5b74faf4eefae78582
beduffy <benfduffy@gmail.com>
2018-12-01 23:43:44 +0000

    Added simplistic bubble chart of most cited papers over time. Custom tooltips. Margin 40px between headings.

--- scripts/explore_semantic_scholar.py
+++ scripts/explore_semantic_scholar.py
@@ -1,3 +1,7 @@
+"""
+Used to create the data for static front end with Chart.js
+"""
+
 from collections import Counter
 import ast
 from pprint import pprint
@@ -53,7 +57,7 @@ def get_all_unique_author_counts(all_paper_objs):
     print(labels)
     print(data)
 
-def get_top_most_cited_papers(all_paper_objs):
+def get_most_cited_papers(all_paper_objs):
     paper_citation_info = [(paper['title'], len(paper['citations']), paper['influentialCitationCount']) for paper in all_paper_objs]
     paper_citation_info = sorted(paper_citation_info, key=lambda x: x[1], reverse=True)
     for p_c_i in paper_citation_info[0:100]:
@@ -66,15 +70,46 @@ def get_top_most_cited_papers(all_paper_objs):
     print(labels)
     print(data)
 
-get_top_most_cited_papers(all_paper_objs)
-get_all_unique_author_counts(all_paper_objs)
+def get_most_cited_papers_over_time(all_paper_objs):
+    paper_citation_info = [
+        (paper['title'], len(paper['citations']), paper['influentialCitationCount'], paper['year']) for paper in
+        all_paper_objs]
+    paper_citation_info = sorted(paper_citation_info, key=lambda x: x[1], reverse=True)
+    for p_c_i in paper_citation_info[0:100]:
+        print(p_c_i)
+
+    top_100 = paper_citation_info[0:100]
+
+    labels = [x[0] for x in top_100]
+    data = [x[1] for x in top_100]
+    print(labels)
+    print(data)
+
+    # todo normalise between 30-70
+    x_y_r = [{'x': x[0], 'y': x[1], 'r': x[1] // 40} for x in top_100 if int(x[3]) >= 2014]
+    print(x_y_r)
+
+    x_y_r = [{'x': x[3], 'y': x[1], 'r': x[1] // 40} for x in top_100 if int(x[3]) >= 2014]
+    print(x_y_r)
+
+    print([x[3] for x in top_100 if int(x[3]) >= 2014])
+    print([x[0] for x in top_100 if int(x[3]) >= 2014])
+    print([x[1] for x in top_100 if int(x[3]) >= 2014])
+    print([x[2] for x in top_100 if int(x[3]) >= 2014])
+
+# get_most_cited_papers(all_paper_objs)
+# get_all_unique_author_counts(all_paper_objs)
+get_most_cited_papers_over_time(all_paper_objs)
 
 
 
-# expected javascript data in form below
+# expected javascript data (bar chart) in form below
 # javascript_data =
 # {"labels":["January","February","March","April","May","June","July"]
 # new Chart(document.getElementById("chartjs-1"),{"type":"bar","data":{"labels":["January","February","March","April","May","June","July"],
 #         "datasets":[{"label":"My First Dataset","data":[65,59,80,81,56,55,40],"fill":false,
 # "backgroundColor":["rgba(255, 99, 132, 0.2)","rgba(255, 159, 64, 0.2)","rgba(255, 205, 86, 0.2)","rgba(75, 192, 192, 0.2)","rgba(54, 162, 235, 0.2)","rgba(153, 102, 255, 0.2)","rgba(201, 203, 207, 0.2)"],"borderColor":["rgb(255, 99, 132)","rgb(255, 159, 64)","rgb(255, 205, 86)","rgb(75, 192, 192)","rgb(54, 162, 235)","rgb(153, 102, 255)","rgb(201, 203, 207)"],"borderWidth":1}]},
 #                                                 "options":{"scales":{"yAxes":[{"ticks":{"beginAtZero":true}}]}}});
+
+# bubble expected:
+# "data":[{"x":20,"y":30,"r":15},{"x":40,"y":10,"r":10}]
