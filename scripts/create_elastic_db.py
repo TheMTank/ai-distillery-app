0eddce3bf520e28a61925a31ad547eb540b997a0
beduffy <benfduffy@gmail.com>
2019-03-30 21:28:18 +0000

    Replaced logging statements with print and put main() into main

--- scripts/create_elastic_db.py
+++ scripts/create_elastic_db.py
@@ -1,11 +1,11 @@
+import argparse
 import os
 import glob
 import pickle
-import logging
+
 from elasticsearch_dsl.connections import connections
 from models import elastic_model
 
-# lets load the existing database to memory
 
 def create_elastic_database(database_path, corpus_path):
     """
@@ -14,12 +14,14 @@ def create_elastic_database(database_path, corpus_path):
     :param corpus_path:
     :return:
     """
+    print('Beginning create_elastic_database function')
+    # load the existing database to memory
     try:
         db = pickle.load(open(database_path, 'rb'))
     except Exception as e:
-        logging.error('error loading existing database:')
-        logging.error(e)
-        logging.error('starting from an empty database')
+        print('error loading existing database:')
+        print(e)
+        print('starting from an empty database')
         db = {}
 
     def get_full_paper_id(full_paper_id):
@@ -39,7 +41,7 @@ def create_elastic_database(database_path, corpus_path):
     full_paper_id_to_date = {get_full_paper_id(doc['id']): doc['published'] for k, doc in db.items()}
     full_paper_id_to_authors = {get_full_paper_id(doc['id']): [x['name'] for x in doc['authors']] for k, doc in db.items()}
 
-    logging.info('{}, {}, {}, {}'.format(len(full_paper_id_to_title), len(full_paper_id_to_abstract),
+    print('{}, {}, {}, {}'.format(len(full_paper_id_to_title), len(full_paper_id_to_abstract),
                                   len(full_paper_id_to_date), len(full_paper_id_to_authors)))
 
     all_papers_full_paper_ids_and_paths = [(x, x.split('/')[-1].split('.pdf')[0]) for x in all_papers_txt_file_paths]
@@ -62,7 +64,7 @@ def create_elastic_database(database_path, corpus_path):
     db_paper_ids_not_in_text_ids = [x for x in list(full_paper_id_to_title.keys()) if x not in all_papers_full_paper_ids]
     text_ids_not_in_db_paper_ids = [x for x in all_papers_full_paper_ids if x not in full_paper_id_to_title]
 
-    logging.info('db_paper_ids_not_in_text_ids len: {}. text_ids_not_in_db_paper_ids len: {}'.format(len(db_paper_ids_not_in_text_ids),
+    print('db_paper_ids_not_in_text_ids len: {}. text_ids_not_in_db_paper_ids len: {}'.format(len(db_paper_ids_not_in_text_ids),
                                                                                               len(text_ids_not_in_db_paper_ids)))
     connections.create_connection(hosts=['localhost'])
 
@@ -72,7 +74,7 @@ def create_elastic_database(database_path, corpus_path):
                         all_papers_full_text, all_papers_authors, all_papers_date)):
 
         if idx % 10 == 0:
-            logging.info('Adding document {}/{}'.format(idx, len(all_paper_titles)))
+            print('Adding document {}/{}'.format(idx, len(all_paper_titles)))
 
         article = elastic_model.Paper(meta={'id': paper_id}, title=title)
         article.abstract = abstract
@@ -86,10 +88,12 @@ def main():
     Usage: python create_elastic_db.py --database /home/data/data_jan.p --corpus /home/data/text
     :return:
     """
-    import argparse
     parser = argparse.ArgumentParser()
-    parser.add_argument("--database", help="databasae path (containing all data)")
-    parser.add_argument("--corpus", help="text corpous path (containing text)")
+    parser.add_argument("--database", help="database path (containing all data)")
+    parser.add_argument("--corpus", help="text corpus path (containing text)")
     args = parser.parse_args()
 
     create_elastic_database(args.database, args.corpus)
+
+if __name__ == '__main__':
+    main()
